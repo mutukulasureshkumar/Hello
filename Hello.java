42b20b3367acb033be3d5f339e28b120cf4f4c39
Mutukula Suresh Kumar <33003278+mutukulasureshkumar@users.noreply.github.com>
2018-05-28 12:56:49 +0530

    Initial commit

---
+++ Hello.java
@@ -0,0 +1,9 @@
+public class Hello {
+	
+	public static void main(String[] args) {
+		for(int i=10; i<20; i++){
+			System.out.println("Hello World!!");
+		}
+	}
+
+}
---
+++ World.java
@@ -0,0 +1,11 @@
+
+public class World {
+
+	public static void main(String[] args) {
+		for(int i=10; i<20; i++){
+			System.out.println("Hello World!!");
+		}
+
+	}
+
+}
